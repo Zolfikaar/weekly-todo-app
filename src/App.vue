5f40633880b063baacbf39e9204296573edc409f
Zolfikaar <fukar.work@gmail.com>
2020-05-09 08:01:16 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <CalendarWeek />
+    <CalendarEntry />
+    <strong>Welcome, this is Weekly Todo App!</strong>
+    <p>It is a simple application, because most human forget alot in general, and the common todo apps are basiclly deppend on monthly prespective. So I assumed the shorter list will be the better for achieving most of theme.</p>
   </div>
 </template>
 
+<script>
+import CalendarWeek from "@/components/CalendarWeek";
+import CalendarEntry from "@/components/CalendarEntry";
+
+export default {
+  name: "app",
+  components: {
+    CalendarWeek,
+    CalendarEntry
+  }
+};
+</script>
+
 <style lang="scss">
+html,
+body {
+  height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  background-color: #6e6e6e;
+  height: inherit;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  strong,
+  p {
+    margin: 0 auto;
+    padding: 10px;
+    color: #cccccc;
+  }
+  strong {
+    margin-top: 10px;
+    display: block;
   }
 }
 </style>
---
+++ src/seed.js
@@ -0,0 +1,57 @@
+export const seedData = [
+    {
+        id: 1,
+        abbvTitle: "Sun",
+        fullTitle: "Sunday",
+        events: [
+            { details: "Get Sweets", edit: false },
+            { details: "Car Wash", edit: false }
+        ],
+        active: true
+    },
+    {
+        id: 2,
+        abbvTitle: "Mon",
+        fullTitle: "Monday",
+        events: [
+            { details: "Get Groceries", edit: false },
+            { details: "Carpool", edit: false }
+        ],
+        active: true
+    },
+    {
+        id: 3,
+        abbvTitle: "Tue",
+        fullTitle: "Tuesday",
+        events: [{ details: "Yoga/Gym", edit: false }],
+        active: false
+    },
+    {
+        id: 4,
+        abbvTitle: "Wed",
+        fullTitle: "Wednesday",
+        events: [{ details: "Work social", edit: false }],
+        active: false
+    },
+    {
+        id: 5,
+        abbvTitle: "Thu",
+        fullTitle: "Thursday",
+        events: [],
+        active: false
+    },
+    {
+        id: 6,
+        abbvTitle: "Fri",
+        fullTitle: "Friday",
+        events: [{ details: "Friday dinner/drinks", edit: false }],
+        active: false
+    },
+    {
+        id: 7,
+        abbvTitle: "Sat",
+        fullTitle: "Saturday",
+        events: [{ details: "Chill with the parents", edit: false }],
+        active: false
+    }
+];
